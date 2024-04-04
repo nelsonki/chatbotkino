706f83d55b811166051e469954272c5b945c1838
nelson quiñonez <rolavetadiuc@gmail.com>
2024-04-04 21:03:44 +0000

    Add new file

---
+++ sett.py
@@ -0,0 +1,24 @@
+
+token ='ubikteca'
+whatsapp_token = 'EAANIEX4HwNcBOwGv9ouMU6ZBcw1O1qoqGopyTXY8SnpK3NoPy7WQ1PbZB2gswqkL1K1kJSp9xkbGEnnJ7xAMMB8xigZBZBZAJbeuTWXmmpaANSZCrr9ZCR7kvZCZAMKs2IhZB3eImuPXVuraqywybHWA2sVlYmaq9eZAFUZCx40JrCcnkDFGfExn6qVlKjB2Icj6'
+whatsapp_url= 'https://graph.facebook.com/v18.0/266317303225066/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+document_premios = "https://tukinotachira.com/system/loteriakino/backend/web/premios/premioswhatsapp.csv"
+document_url_nuevo = "https://tukinotachira.com/wp-content/uploads/2024/02/sorteo37-1-768x983.webp"
+document_url_medio = "https://tukinotachira.com/wp-content/uploads/2024/02/motos-768x960.webp"
+document_url_viejo = "https://tukinotachira.com/wp-content/uploads/2024/02/resuelve1-768x960.webp"
